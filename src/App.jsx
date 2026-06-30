f2ad3c5d4809423fcd617746be403a4c999ece3e
Shreya Mishra <mishrashreya256@gmail.com>
2026-06-30 19:23:53 +0530

    resolving errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+// import Hero from "./components/Hero"
 import Footer from './components/Footer'
 
 const App = () => {
